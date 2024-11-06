7fe608ccb2d3882d1de5a2872abdeb577669b61e
MarkCesium <danikhrenkov29@gmail.com>
2024-11-06 17:32:43 +0300

    (draft) Update structure

--- src/main.py
+++ src/main.py
@@ -1,22 +1,31 @@
-def has_symbol(s: list[str]) -> bool:
+def has_symbol(input_string: str) -> bool:
+    s: list[str] = []
+    for symbol in input_string:
+        s.append(symbol)
     result: bool = False
     result_for_symbol: bool = False
     next_appear: int
+    false_symbols: list[str] = []
     i: int
     j: int
     k: int
 
     for i in range(0, len(s) - 1):
+        if s[i] in false_symbols:
+            continue
         next_appear = i
         result_for_symbol = False
         for j in range(i + 1, len(s)):
-            if s[i] == s[j] and j - next_appear > 1:
-                for k in range(next_appear + 1, j):
-                    if (s[k] == "A") or (s[k] == "B"):
-                        result_for_symbol = True
-                        break
-                    else:
-                        result_for_symbol = False
+            if s[i] == s[j]:
+                if j - next_appear > 1:
+                    for k in range(next_appear + 1, j):
+                        if (s[k] == "A") or (s[k] == "B"):
+                            result_for_symbol = True
+                            break
+                else:
+                    result_for_symbol = False
+                    false_symbols.append(s[j])
+                    break
                 next_appear = j
         if result_for_symbol:
             result = True
@@ -26,11 +35,8 @@ def has_symbol(s: list[str]) -> bool:
 
 def main():
     input_string = input("Введите последовательность символов: ")
-    s: list[str] = []
-    for symbol in input_string:
-        s.append(symbol)
 
-    if has_symbol(s):
+    if has_symbol(input_string):
         print("Данная последовательность СОДЕРЖИТ специальный символ")
     else:
         print("Данная последовательность НЕ СОДЕРЖИТ специальный символ")
